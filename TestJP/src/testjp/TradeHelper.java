c787837bb9fc600c167ff846d6772b3f4557c7db
Alex <chondr2000@yahoo.com>
2016-01-31 19:47:52 +0200

    fix for no stocks in 15 min

--- TestJP/src/testjp/TradeHelper.java
+++ TestJP/src/testjp/TradeHelper.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package testjp;
 
 import java.util.Date;
@@ -16,12 +15,9 @@ import java.util.Map;
  * @author alex
  */
 public class TradeHelper {
-    
-    
-    
-    
-    public  HashMap<String, Double> calcStockPrice(HashSet<Trade> stockset) {
-        
+
+    public HashMap<String, Double> calcStockPrice(HashSet<Trade> stockset) {
+
         Date now = new Date();
         HashMap<String, Helpersum> stockPriceHelper = new HashMap<String, Helpersum>();
 
@@ -30,14 +26,15 @@ public class TradeHelper {
             Helpersum item = new Helpersum();
             double d = stock.getPrice() * stock.getShareq();
             //Check if in past 15 min from now
-            if(now.getTime() - stock.getTimestamp().getTime() > 15*60*1000)
+            if (now.getTime() - stock.getTimestamp().getTime() > 15 * 60 * 1000) {
                 continue;
-            
+            }
+
             if (stockPriceHelper.get(stock.getSymbol()) == null) {
                 item.setPriceq(d);
                 item.setSumquantity(stock.getShareq());
                 stockPriceHelper.put(stock.getSymbol(), item);
-            } else {              
+            } else {
                 item.setPriceq(stockPriceHelper.get(stock.getSymbol()).getPriceq() + d);
                 item.setSumquantity(stockPriceHelper.get(stock.getSymbol()).getSumquantity() + stock.getShareq());
                 stockPriceHelper.put(stock.getSymbol(), item);
@@ -66,8 +63,15 @@ public class TradeHelper {
 
             result = entry.getValue() * result;
         }
+        if (!stocs.isEmpty()) {
+            return Math.pow(result, 1 / stocs.size());
+        } else {
+            System.out.println("You should update trades.txt test data file with trades in the past"
+                    + "15 min");
+            return 0.0;
 
-        return Math.pow(result, 1 / stocs.size());
+        }
+
     }
 
     class Helpersum {
@@ -92,6 +96,5 @@ public class TradeHelper {
         long sumquantity;
 
     }
-    
-    
+
 }
