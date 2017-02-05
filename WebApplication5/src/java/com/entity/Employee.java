291a036837dcae180b453cfe5248de5ad666f76e
root <chondr2000@yahoo.com>
2017-02-05 14:07:30 +0200

    jpa model app

---
+++ WebApplication5/src/java/com/entity/Employee.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.TableGenerator;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author alex
+ */
+@NamedQuery(name="findall.employee",query="select e from Employee e")
+@Entity
+public class Employee implements Serializable {
+    private static final long serialVersionUID = 1L;
+    //@Id
+    //@TableGenerator(allocationSize=1)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+   //@SequenceGenerator(name="Emp_Gen", sequenceName="Emp_Seq",allocationSize=1,initialValue=65)
+    //@GeneratedValue(generator="Emp_Gen",strategy = GenerationType.SEQUENCE)
+    @TableGenerator(name="Emp_Gen",allocationSize=1)
+@Id @GeneratedValue(generator="Emp_Gen")
+    private Long id;
+    private String name;
+
+    public Date getCreationdate() {
+        return creationdate;
+    }
+
+    public void setCreationdate(Date creationdate) {
+        this.creationdate = creationdate;
+    }
+    
+    
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date creationdate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Employee)) {
+            return false;
+        }
+        Employee other = (Employee) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        return "com.entity.Employee[ id=" + id + " ]";
+    }
+    
+}
---
+++ WebApplication5/src/java/com/service/WebServTest.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.service;
+
+import com.entity.Employee;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import static javax.ejb.TransactionAttributeType.NOT_SUPPORTED;
+import javax.jws.WebService;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author alex
+ */
+@Stateless
+@WebService
+public class WebServTest {
+
+    @PersistenceContext(unitName = "WebApplication5PU")
+    private EntityManager em;
+
+    public String insertEmp(String name) {
+
+        Employee emp = new Employee();
+        emp.setName(name);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+
+        emp.setCreationdate(date);
+        em.persist(emp);
+
+        String result;
+        if (findemp(name).isEmpty() == false) {
+            result = " created";
+        } else {
+            result = " not created";
+        }
+
+        return "Employee " + name + result;
+
+    }
+
+//    void persist(Object object) {
+//        em.persist(object);
+//    }
+
+    public List<Employee> findemp(String name) {
+
+        //Employee emp = new Employee();
+        ////emp.setName(name);
+        TypedQuery query = em.createQuery("SELECT e FROM Employee e where e.name like :custName",
+                Employee.class).setParameter("custName", name);
+
+        //emp = em.find(Employee.class, new Long(1));
+        //System.out.print("the id is" + emp.getId() + " the name is " + emp.getName());
+        return query.getResultList();
+
+    }
+
+    @TransactionAttribute(NOT_SUPPORTED)//performance improvement as the entities become detached
+    public List<Employee> findAllEmployees() {
+        //TypedQuery query = em.createQuery("SELECT e FROM Employee e",
+        //      Employee.class);
+
+        TypedQuery query = em.createNamedQuery("findall.employee", Employee.class);
+        return query.getResultList();
+    }
+
+    public List<Employee> findAllCriteria(String name) {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Employee> createQuery = criteriaBuilder.createQuery(Employee.class);
+        Root<Employee> emp = createQuery.from(Employee.class);
+        createQuery.select(emp).where(criteriaBuilder.equal(emp.get("name"), name));
+        //createQuery.select(emp);
+
+        TypedQuery<Employee> q = em.createQuery(createQuery);
+        return q.getResultList();
+    }
+}
