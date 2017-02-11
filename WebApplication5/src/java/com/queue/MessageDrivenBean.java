74b102ec1cb09a000d1a9120f69bd9344ffe7af5
alex <alex@Le-PC>
2017-02-11 13:37:28 +0200

    MDB instance

---
+++ WebApplication5/src/java/com/queue/MessageDrivenBean.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.queue;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ *
+ * @author alex
+ */
+@MessageDriven(mappedName = "TestJMSQueue", activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class MessageDrivenBean implements MessageListener {
+    
+    public MessageDrivenBean() {
+    }
+    
+    @Override
+   public void onMessage(Message inMessage) {
+    TextMessage msg = null;
+
+    try {
+        if (inMessage instanceof TextMessage) {
+            msg = (TextMessage) inMessage;
+            System.out.println("MESSAGE BEAN: Message received: " +
+                msg.getText());
+        } else {
+            System.out.println("Message of wrong type: " +
+                inMessage.getClass().getName());
+        }
+    } catch (JMSException e) {
+        e.printStackTrace();
+        //mdc.setRollbackOnly();
+    } catch (Throwable te) {
+        te.printStackTrace();
+    }
+}
+    
+}
